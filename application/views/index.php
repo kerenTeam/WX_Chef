85b85120e40aed4c83903e6a1212ab34544e64f9
wangfei1234 <1425302858@qq.com>
2016-03-14 14:40:46 +0800

    no message

---
+++ application/views/index.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html class="no-js">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1，user-scalable=0">
+    <title>大厨到家</title>
+    <!-- Set render engine for 360 browser -->
+    <meta name="renderer" content="webkit">
+    <!-- No Baidu Siteapp-->
+    <meta http-equiv="Cache-Control" content="no-siteapp"/>
+    <!-- <link rel="icon" type="image/png" href="assets/i/favicon.png"> -->
+    <link rel="stylesheet" href="../../skin/css/amazeui.min.css">
+    <link rel="stylesheet" href="../../skin/css/app.css">
+  </head>
+  <body>
+    <!-- 搜索 -->
+    <form action="" method="" class="topform">
+      <div class="am-g">
+        <button type="submit" class="am-icon-search sb"></button>
+        <input type="search" class="am-form-field" placeholder="请输入菜名或食材">
+      </div>
+    </form>
+    <!-- banner -->
+    <a href=""><img src="../../skin/img/card.png" class="am-img-responsive card" alt="大厨到家"></a>
+    <!-- 菜品 -->
+    <ul class="am-gallery am-avg-sm-4 am-avg-md-4 am-avg-lg-4 am-gallery-default am-shadow veg" data-am-gallery="{ pureview: true }" >
+      <li>
+        <div class="am-gallery-item">
+          <a href="chef.php">
+            <img src="../../skin/img/menu.png" class="am-circle" alt="大厨到家"/>
+            <h3 class="am-gallery-title red">点菜</h3>
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="am-gallery-item">
+          <a href="">
+            <img src="../../skin/img/fork.png" class="am-circle" alt="大厨到家"/>
+            <h3 class="am-gallery-title yellow">套餐</h3>
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="am-gallery-item">
+          <a href=" ">
+            <img src="../../skin/img/food.png" class="am-circle" alt="大厨到家"/>
+            <h3 class="am-gallery-title green">宴会定制</h3>
+          </a>
+        </div>
+      </li>
+      <li>
+        <div class="am-gallery-item">
+          <a href="">
+            <img src="../../skin/img/tea.png" class="am-circle" alt="大厨到家"/>
+            <h3 class="am-gallery-title pink">燕鲍翅</h3>
+          </a>
+        </div>
+      </li>
+    </ul>
+    <!-- 热销 -->
+    
+    <div class="am-g am-shadow hot" data-am-scrollspy="{animation: 'fade'}">
+    <p class="htit"><span class="am-icon-fire red"></span> 特色服务</p>
+      <div class="am-u-sm-4">
+        <a href="#" class="am-thumbnail">
+          <img class="am-circle" src="../../skin/img/ds.png" width="140" height="140"/>
+          <p class="purple">点杀</p>
+        </a>
+      </div>
+      <div class="am-u-sm-4">
+        <a href="#" class="am-thumbnail">
+          <img class="am-circle" src="../../skin/img/sc.png" width="140" height="140"/>
+          <p class="sc">水产</p>
+        </a>
+      </div>
+      <div class="am-u-sm-4">
+        <a href="#" class="am-thumbnail">
+          <img class="am-circle" src="../../skin/img/cj.png" width="140" height="140"/>
+          <p class="blue">菜价查询</p>
+        </a>
+      </div>
+       <div class="am-u-sm-4">
+        <a href="#" class="am-thumbnail">
+          <img class="am-circle" src="../../skin/img/js.png" width="140" height="140"/>
+          <p class="green">酒水</p>
+        </a>
+      </div>
+      <div class="am-u-sm-4">
+        <a href="#" class="am-thumbnail">
+          <img class="am-circle" src="../../skin/img/hg.png" width="140" height="140"/>
+          <p class="pink">火锅</p>
+        </a>
+      </div>
+      <div class="am-u-sm-4">
+        <a href="#" class="am-thumbnail">
+          <img class="am-circle" src="../../skin/img/kf.png" width="140" height="140"/>
+          <p class="purple">客服</p>
+        </a>
+      </div>
+    </div>
+    <div class="am-shadow fcai">
+      <a href="" class="am-btn am-btn-default am-btn-block bpurple">厨师风采</a>
+      <a href="" class="am-btn am-btn-default am-btn-block bgreen">服务员风采</a>
+      <p class=""><span class="am-icon-eye red"></span> 发现美食</p>
+    </div>
+    <!-- footer -->
+    <div data-am-widget="navbar" class="am-navbar am-cf am-navbar-default" id="">
+      <ul class="am-navbar-nav am-cf am-avg-sm-4 am-shadow">
+        <li >
+          <a href="tel:123456789" class="">
+            <span class="am-icon-phone"></span>
+            <span class="am-navbar-label">呼叫</span>
+          </a>
+        </li>
+        <li data-am-navbar-share>
+          <a href="###" class="">
+            <span class="am-icon-share-square-o"></span>
+            <span class="am-navbar-label">分享</span>
+          </a>
+        </li>
+        <li data-am-navbar-qrcode>
+          <a href="###" class="">
+            <span class="am-icon-qrcode"></span>
+            <span class="am-navbar-label">二维码</span>
+          </a>
+        </li>
+        <li >
+          <a href="https://github.com/allmobilize/amazeui" class="">
+            <span class="am-icon-github"></span>
+            <span class="am-navbar-label">GitHub</span>
+          </a>
+        </li>
+        <li >
+          <a href="http://amazeui.org/getting-started" class="">
+            <span class="am-icon-download"></span>
+            <span class="am-navbar-label">下载使用</span>
+          </a>
+        </li>
+        <li >
+          <a href="https://github.com/allmobilize/amazeui/issues" class="">
+            <span class="am-icon-location-arrow"></span>
+            <span class="am-navbar-label">Bug 反馈</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+    <script src="../../skin/js/jquery.min.js"></script>
+    <!--<![endif]-->
+    <!--[if lte IE 8 ]>
+    <script src="http://libs.baidu.com/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://cdn.staticfile.org/modernizr/2.8.3/modernizr.js"></script>
+    <script src="assets/js/amazeui.ie8polyfill.min.js"></script>
+    <![endif]-->
+    <script src="../../skin/js/amazeui.min.js"></script>
+  </body>
+</html>
