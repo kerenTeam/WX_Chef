b7a3cb61ac2217a36ae8559d11f1e5b367ae57b8
424138799 <424138799@qq.com>
2016-04-07 01:20:04 +0800

    no message

---
+++ application/helpers/post_helper.php
@@ -0,0 +1,23 @@
+<?php 
+
+	function curl_post($url,$data){
+	$options = array(  
+        CURLOPT_RETURNTRANSFER => true,  
+        CURLOPT_HEADER         => false,  
+        CURLOPT_POST           => true,  
+        CURLOPT_POSTFIELDS     => $data,  
+    );  
+  
+    $ch = curl_init($url);  
+    curl_setopt_array($ch, $options);  
+    $result = curl_exec($ch);  
+    curl_close($ch);  
+    return $result;  
+	}
+
+
+
+
+
+
+?>
