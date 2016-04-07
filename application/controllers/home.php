3081d6a8023bd4c7be180bf0ff24ff222110212e
Harris-Aaron <1692853539@qq.com>
2016-04-07 18:16:26 +0800

    no message

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -50,18 +50,22 @@ class home extends CI_Controller
 		$this->load->view('register');
 	}
 
+	public function send(){
+	$phone = $this->input->post('UserPhone');
+    $ch = curl_init();
+    $url = 'http://apis.baidu.com/kingtto_media/106sms/106sms?mobile='.$phone.'&content=%e3%80%90%e5%a4%a7%e5%8e%a8%e5%88%b0%e5%ae%b6%e3%80%91%e6%82%a8%e7%9a%84%e6%b3%a8%e5%86%8c%e9%aa%8c%e8%af%81%e7%a0%81%e4%b8%ba%ef%bc%9a'.randNms;
+    $header = array('apikey: f8ae5ba4094b4d5134303eb87f7a115d');
+    curl_setopt($ch, CURLOPT_HTTPHEADER  , $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch , CURLOPT_URL , $url);
+    $res = curl_exec($ch);
+
+	}
+
 	public function registeradd(){
 		 $reigsterFrom = array('UserPhone' => $this->input->post('UserPhone'),'UserPwd' => $this->input->post('UserPwd'));
          $reigsterData = "[".json_encode($reigsterFrom)."]";
          $isok = curl_post(APIURL."/API_Poorder/Post?dis=User&value=".$reigsterData,'');
-<<<<<<< HEAD
-         // echo "<pre>";
-         // var_dump(APIURL."Post?dis=User&value=".$reigsterData); 
-=======
-         echo "<pre>";
-         var_dump(APIURL."Post?dis=User&value=".$reigsterData); 
->>>>>>> 4be0b1e1c7088efcfa490621e74f3f65a9613676
-         var_dump($isok);
          switch ($isok) { //0注册失败   1注册成功  2已有用户
          	case '0':
          		echo "<script>alert('注册失败！');  window.location.href='register';</script>";  //？注册
@@ -115,11 +119,11 @@ class home extends CI_Controller
 		$a['ShoppingId'] = $_GET['shopping'];  
 		$b = '['. ($a).']';
 		var_dump($b);
-<<<<<<< HEAD
+
 		$c = curl_post(APIURL."/Put?dis=xgcp&value=".$b,'');
-=======
+
 		$c = curl_post(APIURL."Put?dis=xgcp&value=".$b,'');
->>>>>>> b495d60f47e4f3d8daad08a09c46752b34b57266
+
 		var_dump($c);	
 		
 		exit;
@@ -213,11 +217,12 @@ class home extends CI_Controller
 	//购物车 new
 	public function cart(){
 		$cookie = $_COOKIE['phone'];
-<<<<<<< HEAD
+
 		$carts = file_get_contents(APIURL."/Get?dis=gwc&foodid=".$cookie);
-=======
+
 		$carts = file_get_contents(APIURL."Get?dis=gwc&foodid=".$cookie);
->>>>>>> b495d60f47e4f3d8daad08a09c46752b34b57266
+
+
 
 		$list['carts'] = json_decode(json_decode($carts));	
 		$this->load->view('cart',$list);
