0f71676a781046a025356aed0d5e536af695f145
wangfei1234 <1425302858@qq.com>
2016-04-12 12:07:46 +0800

    no message

--- application/views/card.php
+++ application/views/card.php
@@ -23,11 +23,33 @@
   <!-- 可用饭票 --> 
   <div class="am-list-news-bd">
     <ul class="am-list acr">
+      <li class="am-g pflist">
+        <a href="javascript:" class="flexDisplay">
+
+         <div class="am-u-sm-3 am-text-center">
+           <img src="skin/img/cimg.png" class="pnimg" alt="">
+           <div class="am-text-xs gray">主题套餐</div>
+         </div>
+         <div class="am-u-sm-6">
+           <h5 class="black">主题套餐饭票</h5>
+           <div class="am-text-xs gray">使用期限：2016.3.30-2016.4.30</div>
+         </div>
+         <div class="am-u-sm-3 fpr">
+           <!-- <img src="skin/img/fprb.png"> -->
+           <div class="am-text-right fpc">
+             <span class="am-icon-cny"> 5</span>
+             <div>无门槛使用</div>
+           </div>
+         </div>
+
+        </a>
+      </li>
     <?php if(!empty($cards)):?>
       <?php foreach($cards as $val):?>
       <li class="am-g">
         <a href="javascript:"><img src="skin/img/card1.png" alt=""></a>
       </li>
+
     <?php endforeach;?>
       
     <?php else:?>
