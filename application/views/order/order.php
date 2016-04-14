45c3444e96020d6deb36bc07a4b5db2a973da9d7
424138799 <424138799@qq.com>
2016-04-14 19:10:31 +0800

    no message

--- application/views/order/order.php
+++ application/views/order/order.php
@@ -37,12 +37,26 @@
       </ul>
      </div>
          <hr data-am-widget="divider" style="" class="am-divider am-divider-dashed" />
- 
+ <?php if(isset($_SESSION['phone'])){
+        // 总金额
+        $money = array_sum($pricetotal);
+        // 优惠卷
+        $fan = file_get_contents(POSTAPI."API_UserCoupon?UserPhone=".$_SESSION['phone']);
+        $userphone = json_decode(json_decode($fan),true);
+        foreach ($userphone as $key => $value) {
+            if($money > $value['usethreshold']){
+                $usercoupon[$key] = $value;
+            }
+        }
+       
+  }
+  ?>
     <div class="am-shadow am-margin-vertical-sm fpa2">
        <?php if(empty($usercoupon)):?>
             
             <a href="javascript:;" disabled class="am-cf adc">饭票已经用光了哦<span class="am-fr am-icon-xs red">没有饭票 </span></a>
       <?php else:?>
+
            <a class="am-cf adc fclick">饭票可用<?=count($usercoupon);?>张<span class="am-fr am-icon-xs red">选择 <span class="am-icon-angle-down"></span></span></a>
        <div class="am-list-news-bd" id="fpc" style="display: none">
       <ul class="am-list odl"> 
@@ -60,7 +74,7 @@
    <?php if(empty($jifen)):?>
       <a href="javascript:;" class="am-cf adc">积分<span class="am-fr am-icon-xs red">你还没有积分!</span></a>
     <?php else:?>
-       <a href="javascript:;" class="am-cf adc">积分<span class="am-fr am-icon-xs red">300积分已抵用 <span class="am-icon-cny" id='jifenmoney'>30</span> <input type="checkbox" id="jifen"></span></a>
+       <a href="javascript:;" class="am-cf adc">积分<span class="am-fr am-icon-xs red"><span id='diyong'><?=$jifen;?></span>积分已抵用 <span class="am-icon-cny" id='jifenmoney'></span> <input type="checkbox" name='jifen' id="jifen" value="1"></span></a>
     <?php endif;?>
 
       <a href="javascript:;" class="am-cf adc">应付金额<span class="am-fr am-icon-xs am-icon-cny red" id='pricetotal'></span></a>
@@ -79,10 +93,12 @@
          <!--   <a href="<?php echo site_url('home/address2')?>" class="am-cf adc">添加服务地址 <span class="am-icon-angle-right am-fr  am-icon-sm"></span></a> -->
            <input type="hidden" name="UserPhone" value="123456789"/>
 
-           <div class="am-g ammake">
-              <div class="am-u-md-8 am-u-sm-centered">
-                <form class="am-form afcheck" action="<?=site_url('home/addressAdd2');?>" method="post">
-                </form>
+           <div class="am-g ammake am-padding-sm">
+        <input type="text" class="am-form-field am-radius am-margin-bottom-sm" placeholder="请输入用餐桌数" required>
+        <input type="text" class="am-form-field am-radius am-margin-bottom-sm" placeholder="请输入用餐时间" required>
+        <input type="text" class="am-form-field am-radius am-margin-bottom-sm" placeholder="请输入联系方式" required>
+   <br>
+            <button type='button' class="am-fr am-btn am-btn-success" onclick="getorders();">提交</button>
               </div>
             </div>
          <?php else:?>
@@ -121,33 +137,23 @@
 <script src="skin/js/amazeui.min.js"></script>
  <script>
         var payable = '';
+        var jine = '';
+        var discount =0;
         //总金额
+        var dijifen = $('#diyong').text();
+        jine = dijifen/100;
+        $('#jifenmoney').text(jine);
         var amount = $('#money').text();
-        var jifenmoney = $('#jifenmoney').text();
-        payable = amount - jifenmoney;
-        $('#pricetotal').text(payable);
 
+        payable = amount;
+        $('#pricetotal').text(payable);
+        var jifenmoney = 0;
+        // 积分
      
-   
+        
+      
       $(function(){
          // 弹出支付选项弹框
-         $('#pay').click(function() {
-            
-           
-
-          $('body').css('overflow-y','hidden');
-          $('.tkp,.payway').fadeIn(400);
-        });
-         //隐藏弹框
-         $('.tkp').click(function() { 
-          $('body').css('overflow-y','auto');
-          $('.tkp,.payway').fadeOut(400); 
-        });
-         //点击选项后隐藏弹框并调用支付接口
-         $('.payway a').click(function() {
-           $('.tkp,.payway').fadeOut(400);
-           
-         });
 
         $('.fclick').click(function() {
           console.log('.fclick');
@@ -156,30 +162,44 @@
         $('#fpc li').click(function() {
           $('#fpc').slideUp(400);
           $('.fclick').html('饭票<span class="am-fr am-icon-xs red">'+$(this).find('.am-list-item-hd').text()+'<input type="hidden" name="couponid" value="'+$(this).find('#couponid').val()+'" /><span class="am-icon-cny" id="youhui" >'+$(this).find('.am-icon-cny').html()+'</span></span>');
+            discount = $('#youhui').text();
+            payable = amount - discount - jifenmoney;
+            $('#pricetotal').text(payable);
 
-         if($('#jifen').prop("checked")){
-           var discount = $('#youhui').text();
-          payable = amount - discount - jifenmoney;
-          $('#pricetotal').text(payable);
-    
-        }else {
-          var discount = $('#youhui').text();
-          payable = amount - discount;
-          $('#pricetotal').text(payable);
-       }
         });
+
+         $('#jifen').click(function(){
+           if($('#jifen').prop("checked")){
+           // alert(jifenmoney);
+            jifenmoney = $('#jifenmoney').text();
+            payable = amount - discount - jifenmoney;
+            $('#pricetotal').text(payable);
+    
+        }else{
+           payable = amount - discount;
+            $('#pricetotal').text(payable);
+        }
+       
+        })
+       
      
       })
-// function getorders(){
-//             $.ajax({
-//                type: "POST",
-//                url: "<?=site_url('pricesearch/payOrder');?>",
-//                data: $('#formorder').serialize(),
-//                success: function(msg){
-//                   console.log(msg);
-//                }
-//             });
-//}
+
+      var name=$('#name').val();
+      var address=$('#Address').val();
+      var phone=$('#GoodsPhone').val();
+
+
+function getorders(){
+            $.ajax({
+               type: "POST",
+               url: "<?=site_url('pricesearch/payOrder');?>",
+               data: $('#formorder').serialize(),
+               success: function(msg){
+                  console.log(msg);
+               }
+            });
+}
      
 
     </script>
