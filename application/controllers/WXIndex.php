404d3a2626fd06655bdef915c530377c507d8b45
Harris-Aaron <1692853539@qq.com>
2016-04-16 11:14:49 +0800

    WXIndex

--- application/controllers/WXIndex.php
+++ application/controllers/WXIndex.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @Author: Harris-Aaron
  * @Date:   2016-04-15 16:04:05
  * @Last Modified by:   Harris-Aaron
- * @Last Modified time: 2016-04-15 17:01:27
+ * @Last Modified time: 2016-04-15 20:49:35
  */
     
 class WXIndex extends CI_Controller { 
@@ -12,12 +12,16 @@ class WXIndex extends CI_Controller {
 	function __construct()
 	{
         parent::__construct();
-        //$this->load->library('IndexValid');
         $this->load->library('IndexWxApi'); 
+        $this->load->library('IndexValid');
 	}
 	public function index() 
 	{  
-	    $reMenuData = $this->indexwxapi->wxMenuCreate(MENU);
+	 echo "这里是WXIndex控制器,管理WXApi、TOKEN和对话及event反馈";  exit; 
+	}
+	public function menu()
+	{
+		$reMenuData = $this->indexwxapi->wxMenuCreate(MENU);
         if ($reMenuData['errmsg'] == 'ok')
         { echo "<script>alert('自定义菜单修改成功！');</script>";}
         $this->load->view('sdk/lookMe');
