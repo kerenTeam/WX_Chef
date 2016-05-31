b9bd3d944af4de7fa394d8981d6de321223ecd46
Harris-Aaron <1692853539@qq.com>
2016-05-31 15:31:18 +0800

    delete  WXpay

--- webchef/views/shopcar/isOk.php
+++ webchef/views/shopcar/isOk.php
@@ -3,6 +3,12 @@
  * @Author: Harris-Aaron
  * @Date:   2016-05-27 18:27:53
  * @Last Modified by:   Harris-Aaron
- * @Last Modified time: 2016-05-27 18:28:31
+ * @Last Modified time: 2016-05-30 17:31:09
  */
- var_dump($_SESSION['trade_state']); ?>
+ var_dump($_SESSION['trade_state']); 
+var_dump($_SESSION['abcde']);
+		
+var_dump($this->session->userdata('trade_state'));
+var_dump($this->session->userdata('name'));
+
+ ?>
--- webchef/views/shopcar/notify.php
+++ webchef/views/shopcar/notify.php
@@ -1,25 +1,50 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 ini_set('date.timezone','Asia/Shanghai');
 error_reporting(E_ERROR);
-session_start();
-require_once "../lib/WxPay.Api.php";
-require_once '../lib/WxPay.Notify.php';
-require_once 'log.php';
 
-//初始化日志
-$logHandler= new CLogFileHandler("../logs/".date('Y-m-d').'.log');
-$log = Log::Init($logHandler, 15);
+//将微信服务器支付成功的xml数据获取并转化为json
+$resultData = json_encode(json_decode(json_encode(simplexml_load_string(file_get_contents('php://input'), 'SimpleXMLElement', LIBXML_NOCDATA)), true));
 
-class PayNotifyCallBack extends WxPayNotify
-{
+// class PayNotifyCallBack extends WxPayNotify
+// {
+// 	//查询订单
+// 	public function Queryorder($transaction_id)
+// 	{
+// 		$input = new WxPayOrderQuery();
+// 		$input->SetTransaction_id($transaction_id);
+// 		$result = WxPayApi::orderQuery($input);
+// 		if(array_key_exists("return_code", $result)
+// 			&& array_key_exists("result_code", $result)
+// 			&& $result["return_code"] == "SUCCESS"
+// 			&& $result["result_code"] == "SUCCESS")
+// 		{
+// 			return true;
+// 		}
+// 		return false;  //false  取得数据后微信服务器将不再发送返回数据给你，true 则会一直发送给你直到到timeOut
+// 	}
+// }
+
+class PayNotifyCallBack
+{	
+	  public $WxPayNotify;
+      public $CI_Controller;
+
+      function _construct(WxPayNotify $WxPayNotify,CI_Controller $CI_Controller)
+      {
+         $this->WxPayNotify=$WxPayNotify;
+         $this->CI_Controller=$CI_Controller;
+      }
+      function getSession()
+      {
+      	echo "12345678";
+      }
 	//查询订单
 	public function Queryorder($transaction_id)
 	{
 		$input = new WxPayOrderQuery();
 		$input->SetTransaction_id($transaction_id);
 		$result = WxPayApi::orderQuery($input);
-		$_SESSION['trade_state']=json_encode($result);
-		Log::DEBUG("query:".json_encode($result));
 		if(array_key_exists("return_code", $result)
 			&& array_key_exists("result_code", $result)
 			&& $result["return_code"] == "SUCCESS"
@@ -27,28 +52,30 @@ class PayNotifyCallBack extends WxPayNotify
 		{
 			return true;
 		}
-		return false;
-	}
-	
-	//重写回调处理函数
-	public function NotifyProcess($data, &$msg)
-	{
-		Log::DEBUG("call back:" . json_encode($data));
-		$notfiyOutput = array();
-		
-		if(!array_key_exists("transaction_id", $data)){
-			$msg = "输入参数不正确";
-			return false;
-		}
-		//查询订单，判断订单真实性
-		if(!$this->Queryorder($data["transaction_id"])){
-			$msg = "订单查询失败";
-			return false;
-		}
-		return true;
+		return false;  //false  取得数据后微信服务器将不再发送返回数据给你，true 则会一直发送给你直到到timeOut
 	}
 }
 
-Log::DEBUG("begin notify");
+
 $notify = new PayNotifyCallBack();
+file_put_contents('./PayOk.json', $_SESSION['thisPOOrderId'].ABCD, FILE_APPEND | LOCK_EX);
+
+//取出 transaction_id 值
+$transaction = json_decode($resultData,true);
+$transaction_id = $transaction['transaction_id'];
+
+//验证订单支付状态   json里面有 "trade_state":"SUCCESS" 就是支付成功
+$result_transaction_id = PayNotifyCallBack::Queryorder($transaction_id);   
+
+//数据保存在json里面
+file_put_contents('./PayOk.json', PayNotifyCallBack::getSession().$resultData, FILE_APPEND | LOCK_EX);
+$result_transaction_id_array = json_decode($result_transaction_id,true);
+
+//把支付成功的数据发送给数据库，修改数据库该订单的状态
+if ($result_transaction_id_array['trade_state']) {
+	$isPayOk=curl_post(POSTAPI.'API_Poorder?dis=state','{"POOrderId":"'.PayNotifyCallBack::getSession().'","State":1}');
+}else{
+	$isPayOk=curl_post(POSTAPI.'API_Poorder?dis=state','{"POOrderId":"'.PayNotifyCallBack::getSession().'","State":0}');
+}
+
 $notify->Handle(true);
